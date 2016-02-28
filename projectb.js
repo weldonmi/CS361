5953203dd36377bec2be824a17ed178521c24b0b
PrestonPhro <prestonfrom@gmail.com>
2016-02-28 14:17:00 +0900

    quick updates to table creation calls

--- projectb.js
+++ projectb.js
@@ -32,7 +32,7 @@ app.set('port', 3030);
 app.get('/reset-shelter-table',function(req,res,next){
   var context = {};
   pool.query("DROP TABLE IF EXISTS shelter", function(err){ //replace your connection pool with the your variable containing the connection pool
-    var createString = "CREATE TABLE `shelter` ("+
+    var createString = "CREATE TABLE shelter ("+
     "`idShelter` int NOT NULL AUTO_INCREMENT,"+
     "`shelterName` varchar(45) NOT NULL,"+
     "`addressLine1` varchar(45) DEFAULT NULL,"+
@@ -44,7 +44,8 @@ app.get('/reset-shelter-table',function(req,res,next){
 	"PRIMARY KEY (`idShelter`),"+
 	") ENGINE = InnoDB;";
     pool.query(createString, function(err){
-      context.results = "Table reset";
+      context.results = "Shelter table reset";
+	  res.render('home');
     })
   });
 });
@@ -65,6 +66,7 @@ app.get('/reset-user-table',function(req,res,next){
 	") ENGINE = InnoDB;";
     pool.query(createString, function(err){
       context.results = "User table reset";
+	  res.render('home');
     })
   });
 });
