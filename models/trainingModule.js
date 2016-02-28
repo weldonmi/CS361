5cd19b13cbb4bced425181ea6daf793d19427d68
smithg6 <smithg6@oregonstate.edu>
2016-02-27 19:35:53 -0800

    Added hard coded data and columns

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -4,12 +4,31 @@ function getTrainingModulesListing(callback) {
     // work using callbacks. This will cause a little less friction when we move to using
     // the database.
     callback([{
-        name: "How to be nice",
+        name: "How to write a resume",
+        level: "Beginner",
         addedDate: "12-24-2015"
     }, {
-        name: "How to be helpful",
-        addedDate: "12-26-2015"
-    }]);
+        name: "How to ask the best questions",
+        level: "Advanced",
+        addedDate: "2-6-2014"
+    }, {
+        name: "Business Attire",
+        level: "Beginner",
+        addedDate: "12-26-2012"
+    }, {
+        name: "Goal setting and task-orientation",
+        level: "Experienced",
+        addedDate: "3-16-2015"
+    }, {
+        name: "Office Math Skills",
+        level: "Mid-level",
+        addedDate: "6-6-2015"
+    }, {
+        name: "Rights in the workplace",
+        level: "Beginner",
+        addedDate: "8-26-2011"
+    }
+    ]);
 	
 	pool.query("DROP TABLE IF EXISTS xxxx", function(err){ //replace your connection pool with the your variable containing the connection pool
 		var createString = "SELECT ModuleName, AddedDate, LinkToModulePage "+
