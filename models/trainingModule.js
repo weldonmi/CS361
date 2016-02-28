6c1db7a787d9bde3c72f8eef1d1e6d6b5ee77731
Travis Robinson <travisrobinson2006@gmail.com>
2016-02-27 20:40:10 -0800

    Added new function, removed unneeded data field

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -31,48 +31,23 @@ function getTrainingModulesListing(callback) {
 //            context.results = JSON.stringify(rows);
         })
 ;
+}
 
-    
-    
- /*   
-=======
-    callback([{
-        name: "How to write a resume",
-        level: "Beginner",
-        addedDate: "12-24-2015"
-    }, {
-        name: "How to ask the best questions",
-        level: "Advanced",
-        addedDate: "2-6-2014"
-    }, {
-        name: "Business Attire",
-        level: "Beginner",
-        addedDate: "12-26-2012"
-    }, {
-        name: "Goal setting and task-orientation",
-        level: "Experienced",
-        addedDate: "3-16-2015"
-    }, {
-        name: "Office Math Skills",
-        level: "Mid-level",
-        addedDate: "6-6-2015"
-    }, {
-        name: "Rights in the workplace",
-        level: "Beginner",
-        addedDate: "8-26-2011"
-    }
-    ]);
-	
-	pool.query("DROP TABLE IF EXISTS xxxx", function(err){ //replace your connection pool with the your variable containing the connection pool
-		var createString = "SELECT ModuleName, AddedDate, LinkToModulePage "+
-		"FROM modules";
-		pool.query(createString, function(err){
-			context = JSON.stringify(rows);
-			res.send(context);
-		})
-	});
-	*/
-//>>>>>>> origin/master
+function getTrainingModule(callback_individual,module_ID){
+
+    pool.query('SELECT * FROM modules WHERE idModule = ?',[module_ID],  function(err,rows,fields,))
+    {
+            var result = {};
+            result.idModule = rows.idModule;
+            result.name = rows.moduleName;
+            result.ModuleDescription = rows.ModuleDescription;
+            result.ModuleVideo = rows.ModuleVideo;
+            result.AddedDate = rows.AddedDate;
+
+            callback_individual(result);
+    }               
+               
+
 }
 
 module.exports = {
