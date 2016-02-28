3171db2448d2ea0403e18f76b3ab7c0dd7356a12
Travis Robinson <travisrobinson2006@gmail.com>
2016-02-27 19:49:19 -0800

    Changed Callback

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -3,23 +3,30 @@ function getTrainingModulesListing(callback) {
     // The reason this is a callback is because the database connection and stuff will
     // work using callbacks. This will cause a little less friction when we move to using
     // the database.
-    callback([{
-        name: "How to be nice",
-        addedDate: "12-24-2015"
-    }, {
-        name: "How to be helpful",
-        addedDate: "12-26-2015"
-    }]);
-	
-	pool.query("DROP TABLE IF EXISTS xxxx", function(err){ //replace your connection pool with the your variable containing the connection pool
-		var createString = "SELECT ModuleName, AddedDate, LinkToModulePage "+
-		"FROM modules";
-		pool.query(createString, function(err){
-			context = JSON.stringify(rows);
-			res.send(context);
-		})
-	});
-	
+    
+        pool.query('SELECT moduleName, AddedDate FROM modules', function (err, rows, fields)         {
+            if(err){
+                next(err);
+                return;
+            }
+              
+            var result = [];
+            for (var i = 0; i < row.length; i++)
+            {
+                result.push({name : rows[i].moduleName, addedDate : rows[i].AddedDate});
+            }
+              
+              console.log(rows);
+
+              callback(result);    
+    
+//            context.results = JSON.stringify(rows);
+        })
+;
+
+    
+    
+    
 }
 
 module.exports = {
