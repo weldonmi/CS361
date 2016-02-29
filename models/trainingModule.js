2c455fef71fbfdbe996da2d95a1d96094a1c7eff
Rikki Gibson <rikkigibson@gmail.com>
2016-02-28 17:05:52 -0800

    Fix error handling bug and rename columns in query

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -5,59 +5,51 @@ var pool = mysql.createPool({
 	password:	'default',
 	database:	'student'
 });
-// TODO: actually connect to database
-function getTrainingModulesListing(callback) {
-    // The reason this is a callback is because the database connection and stuff will
-    // work using callbacks. This will cause a little less friction when we move to using
-    // the database.
-//<<<<<<< HEAD
-    
-        pool.query('SELECT moduleName, AddedDate FROM modules', function (err, rows, fields)         {
-            if(err){
-                next(err);
-                return;
-            }
-              
-            var result = [];
-            for (var i = 0; i < rows.length; i++)
-            {
-                result.push({name : rows[i].moduleName, addedDate : rows[i].AddedDate});
-            }
-              
-              console.log(rows);
 
-//            getTrainingModule(function(){},1); used for testing
+function getTrainingModulesListing(callback) {
+    pool.query('SELECT name, addedDate FROM module', function (err, rows, fields) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        var result = [];
+        for (var i = 0; i < rows.length; i++) {
+            result.push({ name: rows[i].name, addedDate: rows[i].addedDate });
+        }
+
+        console.log(rows);
+
+//      getTrainingModule(function(){},1); used for testing
             
-              callback(result);    
+        callback(result);    
     
-//            context.results = JSON.stringify(rows);
-        })
-;
+//      context.results = JSON.stringify(rows);
+    });
 }
 
-function getTrainingModule(callback_individual,module_ID){
-
-    pool.query('SELECT * FROM modules WHERE idModule = ?',[module_ID],  function(err,rows,fields)
-    {
+function getTrainingModule(callback_individual, module_ID) {
+    pool.query('SELECT * FROM module WHERE id = ?', [module_ID], function (err, rows, fields) {
         console.log(rows);
-            var result = {idModule : rows[0].idModule, name : rows[0].moduleName, ModuleDescription : rows[0].ModuleDescription, ModuleVideo : rows[0].ModuleVideo, AddedDate : rows[0].AddedDate};
-            
+        var result = {
+            idModule: rows[0].idModule,
+            name: rows[0].moduleName,
+            ModuleDescription: rows[0].ModuleDescription,
+            ModuleVideo: rows[0].ModuleVideo,
+            AddedDate: rows[0].AddedDate
+        };
         
-        
-        
-/*            result.idModule = rows.idModule;
-            result.name = rows.moduleName;
-            result.ModuleDescription = rows.ModuleDescription;
-            result.ModuleVideo = rows.ModuleVideo;
-            result.AddedDate = rows.AddedDate;
+/*      result.idModule = rows.idModule;
+        result.name = rows.moduleName;
+        result.ModuleDescription = rows.ModuleDescription;
+        result.ModuleVideo = rows.ModuleVideo;
+        result.AddedDate = rows.AddedDate;
 */
-            console.log(result);//used to ensure results are correctly output
+        console.log(result);//used to ensure results are correctly output
         
-            callback_individual(result);
-    }               
-               
-
-)}
+        callback_individual(result);
+    });
+}
 
 module.exports = {
     getTrainingModulesListing: getTrainingModulesListing
