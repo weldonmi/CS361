7dfc2e87247b789822d3819c50c91e7a5336fdf5
weldonmi <weldonmi@onid.oregonstate.edu>
2016-02-28 19:22:22 -0800

    Update INSERTS.sql
    Changed order of queries so modulecontent can find valid video entries when being created.

--- INSERTS.sql
+++ INSERTS.sql
@@ -2,13 +2,13 @@ INSERT INTO module (name, description, addedDate) VALUES ('Module 1', 'This is a
 INSERT INTO module (name, description, addedDate) VALUES ('Module 2', 'Preparing for an Interview.', '2016-02-28');
 INSERT INTO module (name, description, addedDate) VALUES ('Module 3', 'Common Interview Questions.', '2016-02-28');
 
-INSERT INTO moduleContent (idModule, idVideo) VALUES (1, 1);
-INSERT INTO moduleContent (idModule, idVideo) VALUES (2, 2);
-INSERT INTO moduleContent (idModule, idVideo) VALUES (3, 3);
-
 INSERT INTO video (name, filePath) VALUES ('Testing Video', 'video1.php');
 INSERT INTO video (name, filePath) VALUES ('Preparation Video', 'videoPrep.php');
 INSERT INTO video (name, filePath) VALUES ('Common Questions Video', 'videoQues.php');
+
+INSERT INTO moduleContent (idModule, idVideo) VALUES (1, 1);
+INSERT INTO moduleContent (idModule, idVideo) VALUES (2, 2);
+INSERT INTO moduleContent (idModule, idVideo) VALUES (3, 3);
 
 INSERT INTO reading (name, content) VALUES ('Test Reading', 'This text should be visible.');
 
@@ -19,8 +19,6 @@ INSERT INTO quizAnswer (answer) VALUES ('second choice');
 INSERT INTO quizQuestion (idMC, idAnswer) VALUES (1, 1);
 
 INSERT INTO quiz (name, idQQ) VALUES ('Quiz 1', 1);
-
-
 
 INSERT INTO shelter (name, addressLine1, addressLine2, city, state, postalCode, phone) VALUES ('Central Shelter for Women', '12 Main St.', 'Suite 2B', 'Los Angeles', 'CA', '95141', '221-221-2221');
 
